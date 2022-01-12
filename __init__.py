4f0238eea22acd587e191aedc74c55cfc6443991
ofiryakobi <90853478+ofiryakobi@users.noreply.github.com>
2022-01-12 19:38:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-__version__="0.1.7"
+__version__="0.1.8"
 from distutils.version import StrictVersion
 import threading
 import urllib.request, json 
@@ -18,8 +18,8 @@ from .saveEstimation import saveEstimation
 
 def versions():
     try:
-		print('**When using DEBM in a paper, please cite: Yakobi, O., & Roth, Y. (2022, January 10). Decision from Experience Behavior Modeling (DEBM): an open-source Python package for developing, evaluating, and visualizing behavioral models. https://doi.org/10.31234/osf.io/3emdw')
-		print('DEBM: Checking for updates')
+        print('**When using DEBM in a paper, please cite: Yakobi, O., & Roth, Y. (2022). Decision from Experience Behavior Modeling (DEBM): an open-source Python package for developing, evaluating, and visualizing behavioral models. https://doi.org/10.31234/osf.io/3emdw')
+        print('DEBM: Checking for updates')
         url=urllib.request.urlopen("https://pypi.org/pypi/DEBM/json")
         data = json.loads(url.read().decode())
         newest_version=StrictVersion(data['info']['version'])
@@ -29,8 +29,8 @@ def versions():
             print('It is important to keep DEBM up-to-date')
             print('Go to the command line and type: pip install debm -U')
             print('****************************************************')
-		else:
-			print('You have the latest DEBM version, no need to update.')
+        else:
+            print('You have the latest DEBM version, no need to update.')
     except:
         print("Could not check for updates, please run 'pip install debm -U' from time to time to make sure you are up-to-date.")
 
